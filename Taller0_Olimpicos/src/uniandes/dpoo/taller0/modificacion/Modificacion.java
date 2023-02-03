863981c238aa7884289e52dc6e5af91a4daaa328
Pres78 <p.restrepor@uniandes.edu.co>
2023-02-02 20:42:30 -0500

    Taller 1

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,22 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+	 public static void main(String[] args) {
+		    System.out.println("Hola Mundo!");
+		    CalculadoraEstadisticas calc = null;
+			try {
+				calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+		    System.out.println(calc.paisConMasMedallistas());
+		  }
+		}
+
+
